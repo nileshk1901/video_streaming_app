2074c29dd7e4c0fa567357736ee98b03d63a1414
nileshk1901 <nileshkush1901@gmail.com>
2024-11-15 23:56:59 +0530

    first commit

---
+++ src/main/java/com/stream/app/controllers/VideoController.java
@@ -0,0 +1,10 @@
+package com.stream.app.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/videos")
+public class VideoController {
+
+}
---
+++ src/main/java/com/stream/app/entities/Video.java
@@ -0,0 +1,29 @@
+package com.stream.app.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="stream_video")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Video {
+    @Id
+    private String videoId;
+    private String title;
+    private String description;
+    private String contentType;
+    private String filePath;
+//    @ManyToOne
+//    private Course course;
+
+}
