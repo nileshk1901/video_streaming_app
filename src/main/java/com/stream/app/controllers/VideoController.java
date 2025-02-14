a1ec2b980a853dab6453f2ae42ee85673b9883b6
nileshk1901 <nileshkush1901@gmail.com>
2025-02-14 22:51:19 +0530

    uploading videos

--- src/main/java/com/stream/app/controllers/VideoController.java
+++ src/main/java/com/stream/app/controllers/VideoController.java
@@ -1,10 +1,45 @@
 package com.stream.app.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.stream.app.entities.Video;
+import com.stream.app.payload.CustomMessage;
+import com.stream.app.services.VideoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/videos")
+@CrossOrigin("http://localhost:5173 ")
 public class VideoController {
+    @Autowired
+    private VideoService videoService;
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestParam("file")MultipartFile file,
+                                    @RequestParam("title") String title,
+                                    @RequestParam("description")String description) {
+        Video video = new Video();
+        video.setTitle(title);
+        video.setDescription(description);
+        video.setVideoId(UUID.randomUUID().toString());
+
+        Video savedVideo = videoService.save(video, file);
+
+        if (savedVideo != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(video);
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(CustomMessage.builder()
+                            .message("Video not uploaded ")
+                            .success(false).build());
+        }
+
+
+    }
+
 
 }
--- src/main/java/com/stream/app/services/VideoServiceImpl.java
+++ src/main/java/com/stream/app/services/VideoServiceImpl.java
@@ -1,17 +1,113 @@
 package com.stream.app.services;
 
 import com.stream.app.entities.Video;
+import com.stream.app.repositories.VideoRepository;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 @Service
 public class VideoServiceImpl implements VideoService{
+
+
+    @Value("${files.video}")
+    String DIR;
+
+
+    private VideoRepository videoRepository;
+
+
+    public VideoServiceImpl(VideoRepository videoRepository) {
+        this.videoRepository = videoRepository;
+    }
+
+    @PostConstruct
+    public void init() {
+
+        File file = new File(DIR);
+
+
+//        try {
+//            Files.createDirectories(Paths.get(HSL_DIR));
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+
+        if (!file.exists()) {
+            file.mkdir();
+            System.out.println("Folder Created:");
+        } else {
+            System.out.println("Folder already created");
+        }
+
+    }
+
     @Override
     public Video save(Video video, MultipartFile file) {
-        return null;
+        // original file name
+
+        try {
+
+            //original file name
+            String filename = file.getOriginalFilename();
+            String contentType = file.getContentType();
+            InputStream inputStream = file.getInputStream();//to read data
+
+
+            // file path
+            String cleanFileName = StringUtils.cleanPath(filename);
+
+
+            //folder path : create
+
+            String cleanFolder = StringUtils.cleanPath(DIR);
+
+
+            // folder path with  filename
+            Path path = Paths.get(cleanFolder, cleanFileName);
+
+            System.out.println(contentType);
+            System.out.println(path);
+
+            // copy file to the folder
+            Files.copy(inputStream, path, StandardCopyOption.REPLACE_EXISTING);
+
+
+            // video meta data
+
+            video.setContentType(contentType);
+            video.setFilePath(path.toString());
+            Video savedVideo = videoRepository.save(video);
+            //processing video
+           // processVideo(savedVideo.getVideoId());
+
+            //delete actual video file and database entry  if exception
+
+            // metadata save
+            return savedVideo;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error in processing video ");
+        }
+
+
     }
+
+
+
 
     @Override
     public Video get(String videoId) {
