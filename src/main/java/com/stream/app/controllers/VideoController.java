5a0682e0307f9a67230b26a278c884ae93a00f49
nileshk1901 <nileshkush1901@gmail.com>
2025-02-18 20:20:51 +0530

    sending videos in chunks

--- src/main/java/com/stream/app/controllers/VideoController.java
+++ src/main/java/com/stream/app/controllers/VideoController.java
@@ -1,19 +1,27 @@
 package com.stream.app.controllers;
 
+import com.stream.app.AppConstants;
 import com.stream.app.entities.Video;
 
 import com.stream.app.playload.CustomMessage;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 
 import com.stream.app.services.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.UUID;
 
@@ -71,5 +79,99 @@ public class VideoController {
 
 
     }
+
+    // stream video in chunks
+    @GetMapping("/stream/range/{videoId}")
+    public ResponseEntity<Resource> streamVideoRange(@PathVariable String videoId, @RequestHeader(value = "Range", required = false) String range) {
+        System.out.println(range);
+
+
+        Video video = videoService.get(videoId);
+        Path path = Paths.get(video.getFilePath());
+
+        Resource resource = new FileSystemResource(path);
+
+        String contentType = video.getContentType();
+
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+
+        }
+
+        //file ki length
+        long fileLength = path.toFile().length();
+
+
+        //pahle jaisa hi code hai kyuki range header null
+        if (range == null) {
+            return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType)).body(resource);
+        }
+
+        //calculating start and end range
+
+        long rangeStart;
+
+        long rangeEnd;
+
+        String[] ranges = range.replace("bytes=", "").split("-");
+        rangeStart = Long.parseLong(ranges[0]);
+
+        rangeEnd = rangeStart + AppConstants.CHUNK_SIZE - 1;
+
+        if (rangeEnd >= fileLength) {
+            rangeEnd = fileLength - 1;
+        }
+
+//        if (ranges.length > 1) {
+//            rangeEnd = Long.parseLong(ranges[1]);
+//        } else {
+//            rangeEnd = fileLength - 1;
+//        }
+//
+//        if (rangeEnd > fileLength - 1) {
+//            rangeEnd = fileLength - 1;
+//        }
+
+
+        System.out.println("Requested Range Header: " + range);
+        System.out.println("File Length: " + fileLength);
+        System.out.println("Parsed rangeStart: " + rangeStart);
+        System.out.println("Parsed rangeEnd: " + rangeEnd);
+
+        InputStream inputStream;
+
+        try {
+
+            inputStream = Files.newInputStream(path);
+            inputStream.skip(rangeStart);
+            long contentLength = rangeEnd - rangeStart + 1;
+
+
+            byte[] data = new byte[(int) contentLength];
+            int read = inputStream.read(data, 0, data.length);
+            System.out.println("read(number of bytes) : " + read);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Range", "bytes " + rangeStart + "-" + rangeEnd + "/" + fileLength);
+            headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+            headers.add("Pragma", "no-cache");
+            headers.add("Expires", "0");
+            headers.add("X-Content-Type-Options", "nosniff");
+            headers.setContentLength(contentLength);
+
+            return ResponseEntity
+                    .status(HttpStatus.PARTIAL_CONTENT)
+                    .headers(headers)
+                    .contentType(MediaType.parseMediaType(contentType))
+                    .body(new ByteArrayResource(data));
+
+
+        } catch (IOException ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+
+    }
+
 
 }
